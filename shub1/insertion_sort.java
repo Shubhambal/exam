5f10e054e8ae1ab8e91423d5d15ce5eed07de7b4
shubham <sb11061997@gmail.com>
2023-02-02 15:39:28 +0530

    bubble_sort.java file added

--- shub1/insertion_sort.java
+++ shub1/insertion_sort.java
@@ -1,1 +1,36 @@
-insertion sort
+public class BubbleSort 
+{
+public static void main(String[]args) 
+{
+  int[] arr = {11,45,17,18,100,25,69};
+  System.out.println("Before Sorting:");
+  printArr(arr);
+  
+  bubbleSort(arr);
+  System.out.println("After Sorting:");
+  printArr(arr);
+}
+static void bubbleSort(int[] arr) 
+{
+  for(int i=0;i<arr.length-1;i++)
+  {
+    for(int j=0;j<arr.length-1;j++)
+    {
+      if(arr[j]>arr[j+1])
+      {
+        int temp=arr[j];
+        arr[j]=arr[j+1];
+        arr[j+1]=temp;
+      }
+    }
+  }
+}
+static void printArr(int[] arr)
+{
+  for(int i:arr)
+  {
+    System.out.print(i+" ");
+  }
+  System.out.println();
+}
+}
